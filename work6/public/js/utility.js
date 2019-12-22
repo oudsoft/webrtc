8b324f0dd8255667fef36fb467dc3b17d8be4248
oudsoft <oudsoft@gmail.com>
2019-12-22 21:03:21 +0700

    update utility

--- work6/public/js/utility.js
+++ work6/public/js/utility.js
@@ -49,11 +49,11 @@ function doCheckRude(data){
 	zero fill
 */
 function zeroFill( number, width ){
-  width -= number.toString().length;
-  if ( width > 0 ){
-    return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
-  }
-  return number + ""; // always return a string
+	width -= number.toString().length;
+	if ( width > 0 ){
+		return new Array( width + (/\./.test( number ) ? 2 : 1) ).join( '0' ) + number;
+	}
+	return number + ""; // always return a string
 }
 
 function padZero(num, size) {
